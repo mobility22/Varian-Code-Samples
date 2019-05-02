615a2b0170a4cf976babad09cb7a6aacefda3063
Yanbing Wang <yanbing.wang@gmail.com>
2019-05-02 09:06:27 -0700

    p id, oncology id, course id. plan id

---
+++ webinars & workshops/21 Apr 2015 Webinar/Eclipse Scripting API/Projects/FindBrachyPlans/FindSRSPlans.cs
@@ -0,0 +1,80 @@
+////////////////////////////////////////////////////////////////////////////////
+//  
+// Copyright (c) 2015 Varian Medical Systems, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in 
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// THE SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+namespace FindBrachyPlans
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Application app = Application.CreateApplication())
+                {
+                    Execute(app);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
+        }
+        static void Execute(Application app)
+        {
+            int countTotal = 0;
+            int countSRS = 0;
+            int total = app.PatientSummaries.Count();
+
+            foreach (PatientSummary ps in app.PatientSummaries.Where(x => (x.Id.Length == 12 && x.Id.StartsWith("1100"))))
+            {
+                //Console.WriteLine(ps.Id);
+                
+                countTotal++;
+                Patient p = app.OpenPatient(ps);
+                foreach (Course c in p.Courses.Where(x => !(x.Id.Contains("QA") || x.Id.Contains("test"))))
+                {
+                    foreach (PlanSetup plan in c.PlanSetups.Where(x => x.PlanType == PlanType.ExternalBeam && x.ApprovalStatus == PlanSetupApprovalStatus.TreatmentApproved))
+                    {
+                        if (plan.NumberOfFractions == 1 && !plan.Id.Contains("#") && !plan.Id.Contains(":"))
+                        {
+                            if (p.PrimaryOncologistId.Length ==7) //4806689 chen; 2573524 koo;
+                            {
+                                countSRS++;
+                                Console.WriteLine(countSRS + "/" + p.Id + "/" + p.PrimaryOncologistId + "/" + c.Id.Replace("/","_") + "/" + plan.Id.Replace("/", "_") + "/" + plan.PlannedDosePerFraction.Dose +
+                                    "/"+ plan.TreatmentApprovalDate +"/"+countTotal + "/" +total);
+                            }
+                        }
+                    }
+                }
+                app.ClosePatient();
+            }
+        }
+    }
+}
